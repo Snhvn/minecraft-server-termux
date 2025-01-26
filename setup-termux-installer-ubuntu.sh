45092ca93aa0025403d2edc28de5286b4fb7f16e
Snhvn <snhvnnguyen@gmail.com>
2025-01-26 09:04:28 +0700

    Update setup-termux-installer-ubuntu.sh

--- setup-termux-installer-ubuntu.sh
+++ setup-termux-installer-ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
 termux-setup-storage
+pkg install x11-repo
 RED='\033[0;31m'
 echo "------------------------"
 echo "SNIPA VN"
@@ -13,16 +14,6 @@ pkg update -y
 pkg install proot-distro
 proot-distro install ubuntu
 clear
-proot-distro login ubuntu
-apt update -y
-apt install sudo
-pkg install wget
-wget https://github.com/Snhvn/minecraft-server-termux/raw/refs/heads/main/setup-termux-minecraft.sh
-exit
-clear
-echo "Đã tải xong tệp setup-minecraft"
-sleep 1
-clear
 echo "------------------------"
 echo "SNIPA VN"
 echo "${RED}YouTube : https://youtube.com/@snipavn205"
@@ -30,5 +21,4 @@ echo "------------------------"
 echo "Đã tải xong"
 echo "Muốn vào ubuntu nhập lệnh 'proot-distro login ubuntu'"
 echo "Muốn thoát chế độ ubuntu nhập lệnh 'exit'để quay lại về termux"
-echo "Muốn cài Minecraft server thì phải trong chế độ ubuntu mới cài được"
-echo "Nếu bạn ở trong chế độ ubuntu thì nhập lệnh này để tải Minecraft 'sudo ./setup-termux-minecraft.sh'"
+echo "Vào res nhập lệnh thứ 2"
