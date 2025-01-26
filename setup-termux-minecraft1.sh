a5e09f007bcb72975c99ae9aab2c12fd04322221
Snhvn <snhvnnguyen@gmail.com>
2025-01-26 09:08:49 +0700

    Update setup-termux-minecraft1.sh

--- setup-termux-minecraft1.sh
+++ setup-termux-minecraft1.sh
@@ -1,1 +1,18 @@
-
+#!/bin/bash
+RED='\033[0;31m'
+echo "------------------------"
+echo "SNIPA VN"
+echo "${RED}YouTube : https://youtube.com/@snipavn205"
+echo "------------------------"
+echo "Lưu Ý vào chế độ ubuntu để cài"
+sleep 2
+apt update -y
+apt install sudo
+pkg install wget
+wget https://github.com/Snhvn/minecraft-server-termux/raw/refs/heads/main/setup-termux-minecraft.sh
+exit
+clear
+echo "Đã tải xong tệp setup-minecraft"
+sleep 1
+echo "Muốn cài Minecraft server thì phải trong chế độ ubuntu mới cài được"
+echo "Nếu bạn ở trong chế độ ubuntu thì nhập lệnh này để tải Minecraft 'sudo ./setup-termux-minecraft.sh'"
