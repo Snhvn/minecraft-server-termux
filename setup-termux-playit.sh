dde0622627e8a049499c0c2c567a4863c2a4f1ab
Snhvn <snhvnnguyen@gmail.com>
2025-01-26 10:08:45 +0700

    Update setup-termux-playit.sh

--- setup-termux-playit.sh
+++ setup-termux-playit.sh
@@ -5,3 +5,11 @@ echo "------------------------"
 echo "SNIPA VN"
 echo "${RED}YouTube : https://youtube.com/@snipavn205"
 echo "------------------------"
+echo "Lưu ý vào chế độ ubuntu rồi cài"
+clear
+echo "Đang tải xuống playit.gg"
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install playit
+echo "Đã tải xong vui lòng nhập 'playit setup'để lấy đường link claim"
