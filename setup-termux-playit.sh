0ce42efe3587defb495ca8fa0f0323778848707e
Snhvn <snhvnnguyen@gmail.com>
2025-01-26 11:32:31 +0700

    Update setup-termux-playit.sh

--- setup-termux-playit.sh
+++ setup-termux-playit.sh
@@ -12,5 +12,10 @@ curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee 
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
 sudo apt install playit
+clear
+echo "------------------------"
+echo "SNIPA VN"
+echo "${RED}YouTube : https://youtube.com/@snipavn205"
+echo "------------------------"
 echo "Đã tải xong vui lòng nhập 'playit setup'để lấy đường link claim"
 echo "Sau khi bạn tạo agents xong rồi add tunnel nhé trên video có hướng dẫn, add tunnel rồi thì quay lại Termux nhập 'playit' để mở tunnel"
