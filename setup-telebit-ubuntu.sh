6f7380bcb095ebe2bdb3ac0e1b4471ede6f82828
Snhvn <snhvnnguyen@gmail.com>
2025-02-19 15:14:23 +0700

    Create setup-telebit-ubuntu.sh

---
+++ setup-telebit-ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+echo "------------------------"
+echo "SNIPA VN"
+echo "YouTube : https://youtube.com/@snipavn205"
+echo "------------------------"
+echo "Lưu ý vào chế độ ubuntu rồi cài"
+echo "Chờ 3s để bắt đầu cài"
+sleep 3
+clear
+echo "   BẮT ĐẦU CÀI   "
+echo "   TẢI XONG NHẬP MAIL"
+curl https://get.telebit.io/ | bash
+clear
+echo "------------------------"
+echo "SNIPA VN"
+echo "YouTube : https://youtube.com/@snipavn205"
+echo "------------------------"
+echo "Đã xong giờ bạn tạo một cái cổng để mở"
+echo "Nhập lệnh này"
+echo "  'telebit http [port]'
+        'telebit tcp [port]'"
